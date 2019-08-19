806be08082682348f2b671b86ba9471ceca00812
Akio-0110 <54272723+Akio-0110@users.noreply.github.com>
2019-08-20 01:41:21 +0900

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -10,10 +10,28 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
 
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
     await ctx.send('goodbye')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def multiply(ctx, a: int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
 bot.run(token)
