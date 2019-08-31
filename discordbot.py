804fca5ac55a4b2291653189694f99dece1f405e
aki <akio.590110@gmail.com>
2019-08-31 21:50:08 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def hello(ctx):
     await ctx.send(f"どうも、{ctx.message.author.name}さん！")
     await ctx.send(f"どうも、{ctx.message.author}さん！")
-    msg = ctx.message.author()
+    msg = ctx.message()
 
 @bot.event
 async def on_command_error(ctx, error):
