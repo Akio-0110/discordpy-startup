62b2cf89cdbcbc1acdb21d246c7ebdd26b5bcd2e
aki <akio.590110@gmail.com>
2019-09-08 02:01:37 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,8 @@
-#from discord.ext import commands
-#import os
-#import traceback
+from discord.ext import commands
+import os
+import traceback
 import discord
-#import asyncio
+import asyncio
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
