4f183d1d5c539077560ef36fd5dc8fa464e83fd7
aki <akio.590110@gmail.com>
2019-10-14 23:39:15 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -3,14 +3,58 @@ import os
 import traceback
 import discord
 import asyncio
+import MySQLdb
+
+
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.command(name="こんにちは")
-async def hello(ctx):
+async def Hello(ctx):
     await ctx.author.send(f"どうも、{ctx.message.author.name}さん！")
     await ctx.author.send(f"どうも、{ctx.message.author}さん！")
+
+@bot.command(name="make")
+async def Make(ctx, arg1):
+    await ctx.channel.send(f"{arg1}を作成しました。")
+    con = MySQLdb.connect(
+        user='b600998caa803a',
+        passwd='7d7aec23',
+        host='us-cdbr-iron-east-02.cleardb.net',
+        db='heroku_0b2656996c2477a')
+    await ctx.channel.send(f"データベースにアクセスしました。")
+    cur = con.cursor()
+    cursor.execute('select * from avalon_data')
+    row = cursor.fetchone()
+
+    await ctx.channel.send(f"game_status = {row}")
+    await ctx.channel.send(f"quest_cnt = {row}")
+    await ctx.channel.send(f"vote_cnt = {row}")
+    await ctx.channel.send(f"game_phase = {row}")
+    await ctx.channel.send(f"game_stop = {row}")
+    game_status = 1
+    quest_cnt = 2
+    vote_cnt = 3
+    game_phase = 4
+    game_stop = 5
+    cursor.excute("insert into avalon_data
+      (game_status, quest_cnt, vote_cnt, game_phase, game_stop)
+        values (%d, %d, %d, %d, %d)",
+            (game_status, quest_cnt, vote_cnt, game_phase, game_stop))
+    cursor.execute('select * from avalon_data')
+    row = cursor.fetchone()
+    await ctx.channel.send(f"game_status = {row}")
+    await ctx.channel.send(f"quest_cnt = {row}")
+    await ctx.channel.send(f"vote_cnt = {row}")
+    await ctx.channel.send(f"game_phase = {row}")
+    await ctx.channel.send(f"game_stop = {row}")
+
+@bot.command(name="f")
+async def Close(ctx, arg1):
+    cursor.close()
+    con.close()
+    await ctx.channel.send(f"データベースを初期化します")
 
 @bot.command(name="in")
 async def hello(ctx):
