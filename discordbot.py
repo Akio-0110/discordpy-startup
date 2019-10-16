2394039d06ce83c7bd4c6491761a657915a57a0b
aki <akio.590110@gmail.com>
2019-10-17 02:36:07 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -48,12 +48,12 @@ token = "DISCORD_BOT_TOKEN"
 #################################
 # ログイン時のアクション
 #################################
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+#@client.event
+#async def on_ready():
+#    print('Logged in as')
+#    print(client.user.name)
+#    print(client.user.id)
+#    print('------')
 
     #################################
     # ヘルプコマンド:?help
