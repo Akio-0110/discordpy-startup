4fd76da7feea76cd6bceff14134448f2fdc4c1db
aki <akio.590110@gmail.com>
2019-11-09 19:01:14 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -1491,7 +1491,7 @@ async def on_message(ctx):
         # init : 初期化
         elif comment == 'c':
             otome_msg = f"赤陣営です"
-            file="./image/*"
+            file="./image/忠*"
             print(glob.glob(avalon_role[0][2]))
             print(glob.glob(file))
             # file=avalon_role[0][2]
