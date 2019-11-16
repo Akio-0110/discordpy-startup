7e19b34e203fc11fd12705751c712e8d71aa7713
aki <akio.590110@gmail.com>
2019-11-16 09:28:08 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -747,6 +747,8 @@ async def on_message(ctx):
                             sql = f"{sql}\n{avalon_role[avalon_role_auto[game_member_num][0][i]][1]}"
                         embed = discord.Embed(title="選択役職",description=sql)
                         await msgch.send(embed=embed)
+                elif len(deck_cmd_match) < game_member_num:
+                    await ctx.channel.send("プレイヤー数より役職の指定数が少ないです。\n合わせたコマンドにしてください。")
                 else:
                     await ctx.channel.send("プレイヤー数と役職の指定数が違います。\n合わせたコマンドにしてください。")
 
