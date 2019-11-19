3883b51a30e43654d260e463278359b75f1f7082
aki <akio.590110@gmail.com>
2019-11-19 21:21:22 +0900

    make it better

--- discordbot.py
+++ discordbot.py
@@ -2806,6 +2806,7 @@ async def on_message(ctx):
 
 @client.event
 async def time_check(ctx):
+    print(ctx.channel.id)
     if ctx.content == '???':
         print(datetime.now().strftime('%H:%M'))
         print(datetime.now().strftime('%w'))
